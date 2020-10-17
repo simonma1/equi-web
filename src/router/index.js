aef4a4b47780ce54019aff3e269b15aad19e77b5
Simon Moniere <simon.moniereabes@gmail.com>
2020-10-16 21:51:46 -0400

    Revert to webHash to fix reload issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
 import Horses from '../views/Horses.vue'
@@ -41,7 +41,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
